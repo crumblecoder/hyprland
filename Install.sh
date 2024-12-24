76736178c08c32f26527b62ce6b2c73b8bd497ce
crumblecoder <crumblecoder@gmail.com>
2024-12-24 21:43:01 +0100

    Error terminal in SSH

--- Install.sh
+++ Install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-sudo xbps-install -Sy wget
+sudo xbps-install -Sy wget fzf parted
 wget -qO- https://raw.githubusercontent.com/crumblecoder/hyprland/main/.void-installer.conf > /tmp/.void-installer.conf
+wget -qO- https://raw.githubusercontent.com/crumblecoder/hyprland/refs/heads/main/CreateDiskPartition > ~/CreateDiskPartition.sh
+lsblk -dpn
+cat ~/CreateDiskPartition.sh| sudo bash
