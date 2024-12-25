42703cfce124155a0f7b32b70b379b8a39eb3dca
crumblecoder <crumblecoder@gmail.com>
2024-12-25 08:31:21 +0100

    Problems with interactive fzf in creatediskpartition

--- Install.sh
+++ Install.sh
@@ -3,4 +3,5 @@ sudo xbps-install -Sy wget fzf parted
 wget -qO- https://raw.githubusercontent.com/crumblecoder/hyprland/main/.void-installer.conf > /tmp/.void-installer.conf
 wget -qO- https://raw.githubusercontent.com/crumblecoder/hyprland/refs/heads/main/CreateDiskPartition > ~/CreateDiskPartition.sh
 lsblk -dpn
+echo "cambio"
 cat ~/CreateDiskPartition.sh| sudo bash
